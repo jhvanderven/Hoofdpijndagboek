aeffa24d16ae6186615849bf544c7b3969ddcb71
jhvanderven <jhvdven@xmsnet.nl>
2012-10-08 21:06:01 +0200

    Left and right head pain points stored separately. Gestures on the head view. Removing points that were already red.

--- src/org/nvh/hoofdpijndagboek/HPDHead.java
+++ src/org/nvh/hoofdpijndagboek/HPDHead.java
@@ -12,6 +12,7 @@ import android.graphics.Paint;
 import android.graphics.Point;
 import android.graphics.Rect;
 import android.os.Bundle;
+import android.view.GestureDetector;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
@@ -20,9 +21,7 @@ import android.view.ViewGroup;
 import com.actionbarsherlock.app.SherlockFragment;
 import com.actionbarsherlock.app.SherlockFragmentActivity;
 
-public class HPDHead extends SherlockFragmentActivity {
-	int mStackLevel = 1;
-
+public class HPDHead extends SherlockFragmentActivity{
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		setTheme(MainActivity.THEME); // Used for theme switching in samples
@@ -30,107 +29,137 @@ public class HPDHead extends SherlockFragmentActivity {
 		setContentView(R.layout.fragment_stack);
 	}
 
-	@Override
-	public void onSaveInstanceState(Bundle outState) {
-		super.onSaveInstanceState(outState);
-		outState.putInt("level", mStackLevel);
-	}
-
 	public static class HurtingFragmentLeft extends HurtingFragment {
 		@Override
 		public View onCreateView(LayoutInflater inflater, ViewGroup container,
 				Bundle savedInstanceState) {
-			HeadView v = new HeadView(getActivity(), R.drawable.waar);
+			HeadView v = new HeadView(getActivity(), R.drawable.waar, this);
 			return v;
 		}
 	}
 
 	public static class HurtingFragmentRight extends HurtingFragment {
-		// TODO: Improve bitmap quality
-		// TODO: Invert the waar bitmap, make it point the other way
 		@Override
 		public View onCreateView(LayoutInflater inflater, ViewGroup container,
 				Bundle savedInstanceState) {
 			HeadView v = new HeadView(getActivity(),
-					R.drawable.waar_mirrored);
+					R.drawable.waar_mirrored, this);
 			return v;
 		}
 	}
 
-	public static class HurtingFragment extends SherlockFragment {
-
-		@Override
-		public void onCreate(Bundle savedInstanceState) {
-			super.onCreate(savedInstanceState);
-		}
+	public static class HurtingFragment extends SherlockFragment{
+		protected List<Point> points = new ArrayList<Point>();
+		protected List<Integer> colors = new ArrayList<Integer>();
+		protected List<Integer> sizes = new ArrayList<Integer>();
 	}
 
-	public static class HeadView extends View {
+	public static class HeadView extends View implements GestureDetector.OnGestureListener, GestureDetector.OnDoubleTapListener{
 		Bitmap b;
 		Paint paint;
 		Rect dest;
-		List<Point> points = new ArrayList<Point>();
-		List<Integer> colors = new ArrayList<Integer>();
-		List<Integer> sizes = new ArrayList<Integer>();
+		private GestureDetector gesturedetector;
+		private HurtingFragment sideOfHead;
 
-		public HeadView(Context context, int pictureID) {
+		public HeadView(Context context, int pictureID, HurtingFragment x) {
 			super(context);
 			Bitmap bImm = BitmapFactory.decodeResource(getResources(),
 					pictureID);
 			b = bImm.copy(Bitmap.Config.ARGB_8888, true);
 			paint = new Paint();
 			dest = new Rect(0, 0, b.getWidth(), b.getHeight());
-		}
-
-		@Override
-		protected void onDraw(Canvas canvas) {
-			super.onDraw(canvas);
-			canvas.drawBitmap(b, null, dest, paint);
-			for (int i = 0; i < points.size(); i++) {
-				paint.setColor(colors.get(i));
-				canvas.drawCircle(points.get(i).x, points.get(i).y,
-						sizes.get(i), paint);
-			}
+			sideOfHead=x;
+	        gesturedetector = new GestureDetector(context, this);
 		}
 
 		@Override
 		public boolean onTouchEvent(MotionEvent event) {
-			if (event.getActionMasked() == MotionEvent.ACTION_DOWN) {
-				Point p = new Point();
-				p.x = (int) event.getX();
-				p.y = (int) event.getY();
-				List<Integer> toDelete = new ArrayList<Integer>();
-				// we want to remove the circle that contains this point
-				// if the user has already drawn something
-				for (int i = 0; i < points.size(); i++) {
-					double square_dist = Math.pow((points.get(i).x - p.x), 2)
-							+ Math.pow((points.get(i).y - p.y), 2);
-					if (square_dist < Math.pow(sizes.get(i), 2)) {
-						// the point is in if its color is red
-						if (colors.get(i) == Color.RED) {
-							toDelete.add(i);
-						}
+			// the trick is to call the onTouchEvent of the detector, not of the super.
+			return gesturedetector.onTouchEvent(event);
+		}
+
+		@Override
+		protected void onDraw(Canvas canvas) {
+//			super.onDraw(canvas);
+			canvas.drawBitmap(b, null, dest, paint);
+			for (int i = 0; i < sideOfHead.points.size(); i++) {
+				paint.setColor(sideOfHead.colors.get(i));
+				canvas.drawCircle(sideOfHead.points.get(i).x, sideOfHead.points.get(i).y,
+						sideOfHead.sizes.get(i), paint);
+			}
+		}
+		@Override
+		public boolean onDoubleTap(MotionEvent e) {
+			return false;
+		}
+
+		@Override
+		public boolean onDoubleTapEvent(MotionEvent e) {
+			return false;
+		}
+
+		@Override
+		public boolean onSingleTapConfirmed(MotionEvent e) {
+			Point p = new Point();
+			p.x = (int) e.getX();
+			p.y = (int) e.getY();
+			List<Integer> toDelete = new ArrayList<Integer>();
+			// we want to remove the circle that contains this point
+			// if the user has already drawn something
+			for (int i = 0; i < sideOfHead.points.size(); i++) {
+				double square_dist = Math.pow((sideOfHead.points.get(i).x - p.x), 2)
+						+ Math.pow((sideOfHead.points.get(i).y - p.y), 2);
+				if (square_dist < Math.pow(sideOfHead.sizes.get(i), 2)) {
+					// the point is in if its color is red
+					if (sideOfHead.colors.get(i) == Color.RED) {
+						toDelete.add(i);
 					}
 				}
-				for (Integer i : toDelete) {
-					// TODO: I do not understand why a remove
-					// does not work. The onDraw does not redraw the
-					// complete screen. Now the user can erase the bitmap.
-					colors.set(i, Color.WHITE);
-					// colors.remove(i);
-					// sizes.remove(i);
-					// points.remove(i);
-				}
-				if (toDelete.size() == 0) {
-					points.add(p);
-					colors.add(Color.RED);
-					sizes.add(getWidth() / 20);
-				}
-				this.invalidate();
-				return true;
 			}
+			for (Integer i : toDelete) {
+				// objects, not integers... and ints are not Integers
+				sideOfHead.colors.remove(sideOfHead.colors.get(i));
+				sideOfHead.sizes.remove(sideOfHead.sizes.get(i));
+				sideOfHead.points.remove(sideOfHead.points.get(i));
+			}
+			if (toDelete.size() == 0) {
+				sideOfHead.points.add(p);
+				sideOfHead.colors.add(Color.RED);
+				sideOfHead.sizes.add(30);
+			}
+			this.invalidate();
+			return true;
+		}
 
-			return super.onTouchEvent(event);
+		@Override
+		public boolean onDown(MotionEvent e) {
+			// handle this one, otherwise we can never draw
+			return true;
+		}
+
+		@Override
+		public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
+				float velocityY) {
+			return false;
+		}
+
+		@Override
+		public void onLongPress(MotionEvent e) {
+		}
+
+		@Override
+		public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX,
+				float distanceY) {
+			return false;
+		}
+
+		@Override
+		public void onShowPress(MotionEvent e) {
+		}
+
+		@Override
+		public boolean onSingleTapUp(MotionEvent e) {
+			return false;
 		}
 	}
 }
